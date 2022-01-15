032e1e77b6200d60c599f49dbda9d28fa5c3332f
shyjuissac <shyjuissacalex@gmail.com>
2022-01-15 13:24:26 +0530

    ''removed

--- pages/api/login.js
+++ pages/api/login.js
@@ -12,7 +12,7 @@ export default async function login(req, res) {
      */
     res.setHeader('Set-Cookie', cookie.serialize('auth', String(data?.login?.authToken ?? ''), {
         httpOnly: true, 
-        secure: ''development'' !== process.env.NODE_ENV,
+        secure: 'development' !== process.env.NODE_ENV,
         path: '/',
         maxAge: 60 * 60 * 24 * 7 // 1 week
     }));
